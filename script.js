08d0f53ee166c175845eb8a68eec3169454cf11a
Millie Cheung <cheungmillie9@gmail.com>
2021-11-15 00:18:07 +0100

    Set up and improve user interface

--- script.js
+++ script.js
@@ -1,31 +1,50 @@
 window.onload = main;
 
 const activeScene = 0;
-const scenes = [ {
-    text: "Choose 1 or 2",
-    nextScene: [1,4,6],
+const scenes = [ { 
+    text: "To start exploring, please let me know your name!",
+    nextScene: [1],
 }, {
-    text: "oj",
+    text: "Welcome " + [name] +", our new robber-to-be. My name is Mill and I am your trainer from InvincibleDestroyers. In order to be a part of us, you have to show us your qualification.",
     nextScene: [0]
 }];
 
 
 function main() {
     renderScene();
+    addEventListener();
 }
 
 function goToNextScene(scene)
 {
-    console.log('goToNextScene')
+    //printText('goToNextScene')
 }
 function renderScene() 
 {
-    console.log('renderScene')
-    prompt(scenes[activeScene].text)
-    let userInput = prompt('select an option')
+    //printText("renderScene")
+    printText(scenes[activeScene].text)
+
+    // let userInput = prompt('select an option')
     
 }
 
+function addEventListener() {
+    const sendButton = document.getElementById("sendResponse");
+    sendButton.onclick = onUserClicksSend
+}
+
+function onUserClicksSend() {
+    let input = document.getElementById("userInput").value;
+    printText(input)
+}
+
+function printText(text) {
+    const div = document.createElement("div")
+    div.className = 'chatbox-system flex mt-4';
+    div.innerHTML = text ;
+
+    document.getElementById("content").appendChild(div);
+}
 
 
 
