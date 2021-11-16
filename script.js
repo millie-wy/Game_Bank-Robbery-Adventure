411ca22500756c717a362d37b9f829bea52f2ef6
Millie Cheung <cheungmillie9@gmail.com>
2021-11-16 12:25:08 +0100

    organised html & css, finalised user interface

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 window.onload = main;
 
-const activeScene = 0;
-const scenes = [ { 
-    text: "To start exploring, please let me know your name!",
-    nextScene: [1],
-}, {
-    text: "Welcome " + [name] +", our new robber-to-be. My name is Mill and I am your trainer from InvincibleDestroyers. In order to be a part of us, you have to show us your qualification.",
-    nextScene: [0]
-}];
+//state 
+let username;
+
+ const activeScene = 0;
+ const scenes = [ { 
+     text: "To start exploring, please let me know your name!",
+     nextScene: [1]
+ }, {
+     text: "Welcome " + username + ", our new robber-to-be. My name is Mill and I am your trainer from InvincibleDestroyers. In order to be a part of us, you have to show us your qualification.",
+     nextScene: [2]
+ }];
+
+//  function firstScene() {
+//      const answer = printText('To start exploring, please let me know your name!')
+//      username = answer; 
+//      secondScene();
+//  }
 
 
 function main() {
+    // firstScene();
     renderScene();
     addEventListener();
 }
@@ -30,21 +40,26 @@ function renderScene()
 
 function addEventListener() {
     const sendButton = document.getElementById("sendResponse");
-    sendButton.onclick = onUserClicksSend
+    sendButton.onclick = onUserClicksSend;
+}
+
+function printText(text) {
+    const divSystem = document.createElement("div-system")
+    divSystem.className = 'chatbox-system flex mt-4';
+    divSystem.innerHTML = text;
+
+    document.getElementById("content").appendChild(divSystem);
 }
 
 function onUserClicksSend() {
     let input = document.getElementById("userInput").value;
-    printText(input)
+    printResponse(input)
 }
 
-function printText(text) {
-    const div = document.createElement("div")
-    div.className = 'chatbox-system flex mt-4';
-    div.innerHTML = text ;
+function printResponse(value) {
+    const divUser = document.createElement("div-user")
+    divUser.className = 'chatbox-user flex mt-4';
+    divUser.innerHTML = value;
 
-    document.getElementById("content").appendChild(div);
+    document.getElementById("content").appendChild(divUser);
 }
-
-
-
